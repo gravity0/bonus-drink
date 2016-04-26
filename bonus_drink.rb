dd540113dad53116dd846fbe26df9add057fd14e
greapflute <greapflutea2@yahoo.co.jp>
2016-04-26 12:43:40 +0900

    テストケース実装

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,9 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    if amount/3 == 0
+      amount%3
+    else
+      (amount - amount%3) + total_count_for((amount/3) + (amount%3))
+    end
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  it "0本購入した場合、0本飲めること" do
+    expect(BonusDrink.total_count_for(0)).to eq 0
+  end
+  it "3本購入した場合、4本飲めること" do
+    expect(BonusDrink.total_count_for(3)).to eq 4
+  end
+  it "11本購入した場合、16本飲めること" do
+    expect(BonusDrink.total_count_for(11)).to eq 16
+  end  
+  it "100本購入した場合、149本飲めること" do
+    expect(BonusDrink.total_count_for(100)).to eq 149
+  end
 end
